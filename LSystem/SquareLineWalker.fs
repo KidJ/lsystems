80a80131372dd65d048c94eb55875e725f4a6147
John Watkins <brainstew7@hotmail.com>
2018-11-11 15:39:06 +0000

    Add square line walker module as example.

---
+++ LSystem/SquareLineWalker.fs
@@ -0,0 +1,29 @@
+﻿namespace LSystem.Systems.Examples
+
+open System.Numerics
+
+open LSystem
+open LSystem.Systems
+
+module SquareLineWalker =
+
+    let parameters : LineWalker.LineWalkerParams =
+        {
+            StartPosition = Utils.makeVector2 0.0f 0.0f
+            Line = Geometry.makeLine Vector2.Zero (Utils.makeVector2 10.0f 0.0f);
+            Theta = 90.0f |> Utils.radians;
+        }
+    
+    let rules =
+            [
+                LSystem.makeRule "F" "F+F-F-FF+F+F-F"
+            ]
+
+    let tokenise (p : LineWalker.LineWalkerParams) = function
+        | 'F' -> [ LineWalker.Line p.Line ]
+        | '-' -> [ LineWalker.Rotate p.Theta ]
+        | '+' -> [ LineWalker.Rotate -p.Theta ]
+        | _ as unmatched -> failwithf "Unrecognised character encountered %A" unmatched
+
+    let make () = 
+        LineWalker.make parameters rules tokenise
